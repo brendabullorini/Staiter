60781caf19aa2b1e8a9faf54e4fd68425922e9d2
Bren <brenda@microblet.com>
2017-11-06 19:44:55 -0300

    Tp2 - con post content solucionado

--- src/main/java/view/PostMb.java
+++ src/main/java/view/PostMb.java
@@ -5,9 +5,9 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import controller.PostController;
 import model.Post;
 import model.User;
+import controller.PostController;
 
 @Named
 public class PostMb{
@@ -21,9 +21,8 @@ public class PostMb{
 	private User user;
 	private String date;
 	private String content;
-		
+	
 	public String add(){
-		System.out.println("/*/*/*/*/**/ CONTENIDO (aca llega null: " + content);
 		Post post = new Post(loginMb.getCurrentUser(), content);
 		postController.createPost(post);		
 		return "index";
@@ -46,14 +45,11 @@ public class PostMb{
 	}
 
 	public String getContent() {
-		System.out.println("Soy el content desde el getter (llega bien) " + content);
 		return content;
 	}
 	
 	public void setContent(String content) {
-		System.out.println("Hola entre al setter (llega bien): " + content);
 		this.content = content;
-		System.out.println("Content seteado: " + this.content);
 	}
 	
 }
